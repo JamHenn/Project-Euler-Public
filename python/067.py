e76b195112f4be9d83ebaa7a89dec9470afdade7
J-Hennigan <jhenniga@tcd.ie>
2019-08-16 22:15:34 +0100

    Python solution to PE 18 and 67

---
+++ python/067.py
@@ -0,0 +1,10 @@
+with open("067.txt", 'r') as f:
+    triangles = [[int(num) for num in line.split()] for line in f]
+
+I = len(triangles)
+for i in range(I-2, -1, -1): # 2nd last row to 1st row
+    for j in range(len(triangles[i])):
+        triangles[i][j] += max(triangles[i+1][j], triangles[i+1][j+1])
+    triangles.pop() # don't need last row anymore
+
+print(triangles)
