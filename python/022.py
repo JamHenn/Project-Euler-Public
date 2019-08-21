a70842047b9729e816ac0255f1c25e05ea7e790d
J-Hennigan <jhenniga@tcd.ie>
2019-08-21 22:57:02 +0100

    Python solution to PE 22

---
+++ python/022.py
@@ -0,0 +1,10 @@
+with open("022.txt", 'r') as f:
+    names = sorted([name.strip('"') for name in f.read().split(',')])
+
+total = 0
+for i, name in enumerate(names):
+    name_value = sum((ord(letter) - 64) for letter in name)
+    name_score = (i + 1) * name_value
+    total += name_score
+
+print(total)
