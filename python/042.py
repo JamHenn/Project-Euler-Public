79e8e75c95a576a398878925599af193d99aa1c6
J-Hennigan <jhenniga@tcd.ie>
2019-12-27 13:01:53 +0000

    Python solution to PE 42

---
+++ python/042.py
@@ -0,0 +1,17 @@
+import math
+
+def is_triangular(n):
+    """
+    Check if the integer n is triangular
+    n is triangular if 8n+1 is a perfect square
+    """
+    return ( math.sqrt( 8*n+1 )%1 == 0.0 )
+
+count = 0
+with open("042.txt", 'r') as f:
+    for word in f.read().split(","):
+        word = word.strip('"')
+        word_value = sum((ord(letter) - 64) for letter in word)
+        count += is_triangular( word_value )
+
+print( count )
